c7f94301c8d646888ae676d9830b20474af5485a
unknown <markomedic91@gmail.com>
2020-01-10 15:38:28 +0100

    tredicesimo commit

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -6,6 +6,9 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
+#define PI 3.14159265359
+
+
 const vec3 lineColor = vec3(0.0, 1.0, 0.0);
 
 float plotLine(vec2 uv, float y){
@@ -28,13 +31,20 @@ void main() {
 
     //float y = quadraticPoint(uv.x, 0.233, 0.340);
     //float y = 1.0 - pow(abs(uv.x), 0.5);
-    float y = 1.0 - pow(abs(uv.x), 1.0);
+    //float y = 1.0 - pow(abs(uv.x), 1.0);
+    //float y = 1.0 - pow(abs(uv.x), 1.5);
+    //float y = 1.0 - pow(abs(uv.x), 5.5);
+    //float y = pow(cos(PI * uv.x / 2.0), 3.0);
+    //float y = 1.0 - pow(abs(sin(PI * uv.x / 2.0)), 3.0);
+    //float y = pow(min(cos(PI * uv.x / 2.0), 1.0 - abs(uv.x)), 2.5);
+    //float y = 1.0 - pow(max(0.0, abs(uv.x) * 2.0 - 1.0), 0.5);
+    float y = 1.0 - pow(max(0.0, abs(uv.x) * 2.0 - 1.0), 0.5);
 
     vec3 gradient = vec3(y);
 
     float line = plotLine(uv, y);
 
-    vec3 color = (1.0 - line) * gradient + line * lineColor;
+    vec3 color = (1.0 - line) + line * lineColor;
 
     gl_FragColor = vec4(color, 1.0);
 }
