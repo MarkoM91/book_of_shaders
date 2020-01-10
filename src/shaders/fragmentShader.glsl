4d750d0094e32c834b92a1bbaa80f1f9048b88d1
unknown <markomedic91@gmail.com>
2020-01-10 10:09:32 +0100

    sesto commit

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -27,16 +27,35 @@ float cosineApprox(float x){
     return fa * x6 - fb * x4 + fc * x2;
 }
 
+float doubleCubicSeat(float x, float a, float b) {
+  float epsilon = 0.00001;
+  float min_param_a = 0.0 + epsilon;
+  float max_param_a = 1.0 - epsilon;
+  float min_param_b = 0.0;
+  float max_param_b = 1.0;
+
+  a = min(max_param_a, max(min_param_a, a));
+  b = min(max_param_b, max(min_param_b, b));
+
+  float y = 0.0;
+  if(x <= a)  {
+    y = b - b * pow(1.0 - x / a, 3.0);
+  } else {
+    y = b + (1.0 - b) * pow((x - a) / (1.0 -  a), 3.0);
+  }
+  return y;
+}
+
 
 void main() {
 	vec2 uv = gl_FragCoord.xy / u_resolution.xy;
 
     //float y = cosineApprox(uv.x);
 
-    //float y = doubleCubicSeat(uv.x, 0.407, 0.720);
+    float y = doubleCubicSeat(uv.x, 0.407, 0.720);
     //float y = doubleCubicSeat(uv.x, 0.607, 0.247);
 
-    float y = cosineApprox(uv.x);
+    //float y = cosineApprox(uv.x);
     //float y = doubleCubicSeatLinear(uv.x, 0.347, 0.887);
 
     //float y = doublePolynomialSigmoid(uv.x, 0.347, 0.887, 2.0);
