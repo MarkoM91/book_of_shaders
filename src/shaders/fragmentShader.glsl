47553616764f6e385e4292fba63b50b2f09fd3a6
unknown <markomedic91@gmail.com>
2020-01-10 17:49:38 +0100

    venti commit

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -2,37 +2,40 @@
 precision mediump float;
 #endif
 
-#define PI 3.14159265359
-
 uniform vec2 u_resolution;
-uniform vec2 u_mouse;
 uniform float u_time;
 
-vec3 colorA = vec3(0.8,0.43,0.05); //80% 43%  5%
-vec3 colorB = vec3(0.73,0.7,0.32); //73% 70% 32%
-
-float plot (vec2 st, float pct){
-  return  smoothstep( pct-0.01, pct, st.y) -
-          smoothstep( pct, pct+0.01, st.y);
+vec3 rgb2hsb( in vec3 c ){
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz),
+                 vec4(c.gb, K.xy),
+                 step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r),
+                 vec4(c.r, p.yzx),
+                 step(p.x, c.r));
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)),
+                d / (q.x + e),
+                q.x);
 }
 
-void main() {
-    vec2 st = gl_FragCoord.yx/u_resolution.xy;
+vec3 hsb2rgb( in vec3 c ){
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
+                             6.0)-3.0)-1.0,
+                     0.0,
+                     1.0 );
+    rgb = rgb*rgb*(3.0-2.0*rgb);
+    return c.z * mix(vec3(1.0), rgb, c.y);
+}
+
+void main(){
+    vec2 st = gl_FragCoord.xy/u_resolution;
     vec3 color = vec3(0.0);
 
-    vec3 pct = vec3(st.x);
-
-//     pct.r = smoothstep(0.0,1.0, st.x);
-    pct.r = sin(st.x*PI-PI/2.);
-    pct.g = sin(st.x*PI);
-    pct.b = sin(st.x*PI + PI/2.);
-
-    color = mix(colorA, colorB, pct);
-
-    // Plot transition lines for each channel
-    color = mix(color,vec3(1.0,0.0,0.0),plot(st,pct.r));
-    color = mix(color,vec3(0.0,1.0,0.0),plot(st,pct.g));
-    color = mix(color,vec3(0.0,0.0,1.0),plot(st,pct.b));
+    // We map x (0.0 - 1.0) to the hue (0.0 - 1.0)
+    // And the y (0.0 - 1.0) to the brightness
+    color = hsb2rgb(vec3(st.x,1.0,st.y));
 
     gl_FragColor = vec4(color,1.0);
 }
