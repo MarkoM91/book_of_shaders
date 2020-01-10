d9da39feffcf6b168c4c95ff333785d6a9b168b3
unknown <markomedic91@gmail.com>
2020-01-10 17:30:22 +0100

    dicianove commit

--- fragmentShader11.glsl
+++ fragmentShader11.glsl
@@ -11,21 +11,20 @@ uniform float u_time;
 vec3 colorA = vec3(0.8,0.43,0.05); //80% 43%  5%
 vec3 colorB = vec3(0.73,0.7,0.32); //73% 70% 32%
 
-float plot (vec2 st, float pct){
+float plot (vec2 st, float pct) {
   return  smoothstep( pct-0.01, pct, st.y) -
           smoothstep( pct, pct+0.01, st.y);
 }
 
 void main() {
-    vec2 st = gl_FragCoord.yx/u_resolution.xy;
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
     vec3 color = vec3(0.0);
 
     vec3 pct = vec3(st.x);
 
-//     pct.r = smoothstep(0.0,1.0, st.x);
-    pct.r = sin(st.x*PI-PI/2.);
-    pct.g = sin(st.x*PI);
-    pct.b = sin(st.x*PI + PI/2.);
+    //pct.r = sin(st.x*PI-PI/2.);
+    //pct.g = sin(st.x*PI);
+    //pct.b = sin(st.x*PI + PI/2.);
 
     color = mix(colorA, colorB, pct);
 
