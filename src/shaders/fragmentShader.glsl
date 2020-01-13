88d79b7e603c05e87b47eb6dbb6fd13ddbfcfa07
unknown <markomedic91@gmail.com>
2020-01-13 17:29:23 +0100

    trentasette

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -7,6 +7,7 @@ uniform float u_time;
 
 void main(){
     vec2 uv = gl_FragCoord.xy/u_resolution.xy;
+  //  vec3 color = vec3(0.0);
     float pct = 0.0;
 
     // a. The DISTANCE from the pixel to the center
@@ -14,13 +15,13 @@ void main(){
 
      //b. The LENGTH of the vector
     //    from the pixel to the center
-    // vec2 toCenter = vec2(0.5)-uv;
-    // pct = length(toCenter);
+     vec2 toCenter = vec2(0.5)-uv;
+     pct = length(toCenter);
 
     // c. The SQUARE ROOT of the vector
     //    from the pixel to the center
-     vec2 tC = vec2(0.5)-uv;
-     pct = sqrt(tC.x*tC.x+tC.y*tC.y);
+     //vec2 tC = vec2(0.5)-uv;
+     //pct = sqrt(tC.x*tC.x+tC.y*tC.y);
 
     vec3 color = vec3(pct);
 
