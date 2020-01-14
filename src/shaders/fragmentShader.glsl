732309a4447377c08b6afb6505dae03280859d58
unknown <markomedic91@gmail.com>
2020-01-14 12:46:00 +0100

    commit quarantotto

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -7,20 +7,26 @@ uniform float u_time;
 
 void main() {
   vec2 st = gl_FragCoord.xy/u_resolution.xy;
-  vec2 backgroundColor = vec3(1.0);
-  vec3 axesColor = vec3(0.0, 0.0, 1.0);
-  vec3 gridColor = vec3(0.5);
+  vec3 backgroundColor = vec3(1.0);
+	vec3 axesColor = vec3(0.0, 0.0, 1.0);
+	vec3 gridColor = vec3(0.5);
 
-  vec3 pixel = backgroundColor;
+	// start by setting the background color. If pixel's value
+	// is not overwritten later, this color will be displayed.
+	vec3 pixel = backgroundColor;
 
-  const float tickWidth = 0.1;
-  for(float i = 0.0; i < 1.0; i += tickWidth)  {
-    if(abs(st.x - i) < 0.002) pixel = gridColor;
-    if(abs(st.y - i) < 0.002) pixel = gridColor;
-  }
-
-  if(abs(st.x) < 0.005) pixel = axesColor;
-  if(abs(st.y) < 0.006) pixel = axesColor;  
+	// Draw the grid lines
+	// we used "const" because loop variables can only be manipulated
+	// by constant expressions.
+	const float tickWidth = 0.1;
+	for(float i=0.0; i<1.0; i+=tickWidth) {
+		// "i" is the line coordinate.
+		if(abs(st.x - i)<0.002) pixel = gridColor;
+		if(abs(st.y - i)<0.002) pixel = gridColor;
+	}
+	// Draw the axes
+	if( abs(st.x)<0.005 ) pixel = axesColor;
+	if( abs(st.y)<0.006 ) pixel = axesColor;
 
   gl_FragColor = vec4(pixel, 1.0);
 }
